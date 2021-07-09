59f05d39ea08c8722210f9fdd366c095cf6a4f0b
Martin Guillon <martin.guillon@akylas.fr>
2021-07-09 17:11:26 +0200

    fix: map component working

--- common/components/Map.ts
+++ common/components/Map.ts
@@ -3,7 +3,6 @@ import PageComponent from './PageComponent';
 import { ComponentIds } from './App';
 import InteractiveMap from './InteractiveMap';
 import Vue, { NativeScriptVue, NavigationEntryVue } from 'nativescript-vue';
-import { LoggedinEvent, LoggedoutEvent } from '../services/AuthService';
 import { mdiFontFamily } from '../variables';
 
 @Component({
@@ -14,23 +13,12 @@ import { mdiFontFamily } from '../variables';
 export default class Map extends PageComponent {
     navigateUrl = ComponentIds.Map;
     mdiFontFamily = mdiFontFamily;
-    public currentlyLoggedIn = Vue.prototype.$authService.isLoggedIn();
 
     mounted(): void {
         super.mounted();
-        this.$authService.on(LoggedinEvent, this.onLoggedIn, this);
-        this.$authService.on(LoggedoutEvent, this.onLoggedOut, this);
     }
     destroyed(): void {
         super.destroyed();
-        this.$authService.on(LoggedinEvent, this.onLoggedIn, this);
-        this.$authService.on(LoggedoutEvent, this.onLoggedOut, this);
-    }
-    onLoggedIn(e?) {
-        this.currentlyLoggedIn = true;
-    }
-    onLoggedOut() {
-        this.currentlyLoggedIn = false;
     }
 
     goToLogin() {
