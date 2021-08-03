718ed8ddadda23a1bf4a599e75e825dd3f9080b2
Martin Guillon <martin.guillon@akylas.fr>
2021-08-03 11:14:20 +0200

    chore: text transform for button with icon

--- common/components/ButtonWithIcon.vue
+++ common/components/ButtonWithIcon.vue
@@ -1,7 +1,7 @@
 <template>
-    <Button @tap="$emit('tap', $event)" textAlignment="center" verticalTextAlignment="center" paddingTop="7" :color="color">
+    <Button @tap="$emit('tap', $event)" textAlignment="center" verticalTextAlignment="center" paddingTop="7" :color="color" ios:paddingBottom="3">
         <Span :fontSize="vertical ? 14 : 26" :fontFamily="mdiFontFamily" :text="icon" :visibility="icon ? 'visible' : 'collapsed'" verticalAlignment="center" />
-        <Span :text="((vertical ? '\n' : '  ') + text) | uppercase" verticalAlignment="center" :fontSize="vertical ? 12 : 17" />
+        <Span :text="(vertical ? '\n' : '  ') + text" :textTransform="textTransform" verticalAlignment="center" :fontSize="vertical ? 12 : 17" />
     </Button>
 </template>
 <script lang="ts">
@@ -15,6 +15,7 @@ export default class ButtonWithIcon extends Vue {
     mdiFontFamily = mdiFontFamily;
     appFontFamily = appFontFamily;
     colorAccent = colorAccent;
+    @Prop({ type: String, default:'uppercase' }) textTransform: string;
     @Prop({ type: String }) text: string;
     @Prop({ type: String }) icon: string;
     @Prop() color: string | Color;
