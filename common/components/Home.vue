2695f053c8fa8e7a9921c0cd7ea2a381d5303824
Martin Guillon <martin.guillon@akylas.fr>
2021-08-03 11:14:09 +0200

    fix: home layout fi

--- common/components/Home.vue
+++ common/components/Home.vue
@@ -4,14 +4,14 @@
         <GridLayout @tap="goToMap" />
 
         <StackLayout class="flatCardView" elevation="10" borderRadius="25" margin="0 40 50 40" v-show="!currentlyLoggedIn" verticalAlignment="bottom">
-            <CanvasLabel height="100" padding="20" @tap="() => {}">
-                <CGroup :color="colorAccent" lineHeight="30">
-                    <CSpan text="mdi-check" :color="colorPrimary" fontSize="22" :fontFamily="mdiFontFamily" verticalTextAlignment="center" />
-                    <CSpan :text="' ' + $tc('apy_my_purchases')" fontSize="16" fontWeight="bold" verticalTextAlignment="center" />
-                    <CSpan :text="'\n' + 'mdi-check'" :color="colorPrimary" fontSize="22" :fontFamily="mdiFontFamily" verticalTextAlignment="center" />
-                    <CSpan :text="' ' + $tc('send_request_money')" fontSize="16" fontWeight="bold" verticalTextAlignment="center" />
-                </CGroup>
-            </CanvasLabel>
+            <Label padding="20" @tap="() => {}" :color="colorAccent" lineHeight="10" textWrap="true">
+                <!-- <CGroup :color="colorAccent" lineHeight="30"> -->
+                <Span text="mdi-check" :color="colorPrimary" fontSize="22" :fontFamily="mdiFontFamily" verticalTextAlignment="center" />
+                <Span :text="' ' + $tc('apy_my_purchases')" :color="colorAccent" fontSize="16" fontWeight="bold" verticalTextAlignment="center" />
+                <Span :text="'\n' + 'mdi-check'" :color="colorPrimary" fontSize="22" :fontFamily="mdiFontFamily" verticalTextAlignment="center" />
+                <Span :text="' ' + $tc('send_request_money')" :color="colorAccent" fontSize="16" fontWeight="bold" verticalTextAlignment="center" />
+                <!-- </CGroup> -->
+            </Label>
             <Button :text="$t('login')" @tap="goToLogin" margin="0" elevation="0" dynamicElevationOffset="0" />
         </StackLayout>
         <StackLayout class="flatCardView" elevation="10" borderRadius="25 25 0 0" margin="0 40 0 40" v-show="currentlyLoggedIn" verticalAlignment="bottom" padding="24 16 24 16" @tap="() => {}">
@@ -24,7 +24,7 @@
                 </Label>
                 <Button col="1" variant="text" :text="$tc('credit')" horizontalAlignment="right" />
                 <ButtonWithIcon colSpan="2" row="1" icon="mdi-qrcode-scan" :text="$tc('pay_my_purchases')" @tap="goToPay" />
-                <ButtonWithIcon variant="text" colSpan="2" row="2" icon="mdi-swap-horizontal" :text="$tc('send_request')" @tap="goToSendRequest" />
+                <ButtonWithIcon colSpan="2" row="2" icon="mdi-swap-horizontal" :text="$tc('send_request')" @tap="goToSendRequest" />
             </GridLayout>
         </StackLayout>
     </BasePage>
